4194cf18d9be1a4f9052971d90d1e402d3007203
souzamirella <99846689+souzamirella@users.noreply.github.com>
2022-05-13 14:01:05 -0300

    Update challenge-08.js

--- challenge-08/challenge-08.js
+++ challenge-08/challenge-08.js
@@ -3,26 +3,36 @@ Declare uma variável chamada `sum` e atribua a ela uma função chamada
 `calculateSum`. A função deve receber dois parâmetros e retornar a soma
 desses parâmetros.
 */
-// ?
+var sum = function calculateSum (x,y){
+    return x +y ;
+}
+
 
 /*
 Invoque a função criada acima, passando dois números que serão somados, e mostre
 o resultado no console, com a frase:
 "A soma de [VALOR 1] e [VALOR2] é igual a [RESULTADO]."
 */
-// ?
-
+var sum = function calculateSum (x,y){
+    var sum2 = x + y
+console.log ('A soma de '+ x + ' e ' + y +' é igual a '+ sum2 +'.');
+}
 /*
 Mostre no console o nome da função criada acima, com a frase:
 "O nome da função que faz a soma é [NOME DA FUNÇÃO]."
 */
-// ?
+var sum = function calculateSum (x,y){
+    var sum2 = x + y
+    console.log ('O nome da função que faz a soma é ' + calculateSum.name + ' . ');
+}
 
 /*
 Crie uma função literal chamada `showName`. Essa função deve retornar o
 seu nome.
 */
-// ?
+function showName (name){
+    return "Mirella"
+}
 
 /*
 Declare uma variável chamada `varShowName` que recebe a função criada acima.
